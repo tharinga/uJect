443b03f7cde2d5ec7eb4e5c17d98c0cf3f8a50dc
tharinga <timonster@gmail.com>
2020-10-18 17:39:44 +0700

    Create unit tests

--- Assets/Tests/InjectTests.cs
+++ Assets/Tests/InjectTests.cs
@@ -1,20 +1,99 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 using UnityEngine;
+using uJect;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
 
 namespace Tests
 {
     public class InjectTests
     {
-        [Test]
-        public void Inject_Monobehavior_From_Scene_NotNull()
+        private Container _container;
+        private List<MonoBehaviour> _destroyables = new List<MonoBehaviour>();
+        
+        TTarget ArrangeSourceAndTarget<TSource, TTarget>() 
+            where TTarget : MonoBehaviour 
+            where TSource : MonoBehaviour 
         {
-            var target = new GameObject("Target").AddComponent<InjectionTarget>();
-            var source = new GameObject("Source").AddComponent<InjectionSource>();
-            var container = new GameObject("Container").AddComponent<Container>();
+            var target = new GameObject("Target").AddComponent<TTarget>();
+            var source = new GameObject("Source").AddComponent<TSource>();
+            _destroyables.Add(target);
+            _destroyables.Add(source);
+            return target;
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _container = new GameObject("Container").AddComponent<Container>();
+        }
+        
+        [Test]
+        public void Inject_MonoBehaviour_From_Scene_Into_Target_With_Concrete_Dependency()
+        {
+            var target = ArrangeSourceAndTarget<InjectionSource, InjectionTargetWithConcreteDependency>();
             
-            container.Bind<InjectionSource>();
+            _container.Bind<InjectionSource>().AsSingle();
+            _container.ResolveDependencies();
             
-            Assert.That(target.Value, Is.EqualTo(10));
+            Assert.That(target.Value, Is.EqualTo("A"));
+        }
+        
+        [Test]
+        public void Inject_MonoBehaviour_From_Scene_Into_Target_With_Interface_Dependency()
+        {
+            var target = ArrangeSourceAndTarget<InjectionSource, InjectionTargetWithInterfaceDependency>();
+            
+            _container.Bind<IInjectionSource>().To<InjectionSource>().AsSingle();
+            _container.ResolveDependencies();
+            
+            Assert.That(target.Value, Is.EqualTo("A"));
+        }
+        
+        [Test]
+        public void Inject_Multiple_MonoBehaviours_Into_Multiple_Targets()
+        {
+            var target1 = ArrangeSourceAndTarget<InjectionSource, InjectionTargetWithConcreteDependency>();
+            var target2 = ArrangeSourceAndTarget<InjectionSource, InjectionTargetWithInterfaceDependency>();
+            
+            _container.Bind<InjectionSource>().AsSingle();
+            _container.Bind<IInjectionSource>().To<InjectionSource>().AsSingle();
+            _container.ResolveDependencies();
+            
+            Assert.That(target1.Value, Is.EqualTo("A"));
+            Assert.That(target2.Value, Is.EqualTo("A"));
+        }
+        
+        [Test]
+        public void Inject_Conditional_Into_Specified_Target()
+        {
+            var target1 = ArrangeSourceAndTarget<InjectionSource, InjectionTargetWithInterfaceDependency>();
+            var target2 = ArrangeSourceAndTarget<AlternateInjectionSource, AlternateInjectionTargetWithInterfaceDependency>();
+            
+            _container.Bind<IInjectionSource>().To<InjectionSource>().WhenInjectedInto<InjectionTargetWithInterfaceDependency>().AsSingle();
+            _container.Bind<IInjectionSource>().To<AlternateInjectionSource>().WhenInjectedInto<AlternateInjectionTargetWithInterfaceDependency>().AsSingle();
+            _container.ResolveDependencies();
+            
+            Assert.That(target1.Value, Is.EqualTo("A"));
+            Assert.That(target2.Value, Is.EqualTo("B"));
+        }
+        
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {
+            Debug.Log("Teardown");
+            foreach (var monoBehaviour in _destroyables)
+            {
+                Object.DestroyImmediate(monoBehaviour.gameObject);
+            }
+            Object.DestroyImmediate(_container.gameObject);
+            _destroyables.Clear();
+            yield return null;
         }
     }
 }
